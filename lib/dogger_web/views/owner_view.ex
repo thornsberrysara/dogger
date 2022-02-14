7c426da99c0e011352d20cb8ba410851f3801e10
thornsberrysara <thornsberrysara@gmail.com>
2022-02-14 11:47:00 -0500

    Capitalizing owner and pet names

--- lib/dogger_web/views/owner_view.ex
+++ lib/dogger_web/views/owner_view.ex
@@ -5,7 +5,16 @@ defmodule DoggerWeb.OwnerView do
     area_code_string = String.slice(phone_number, 0..2)
     exchange_code_string = String.slice(phone_number, 3..5)
     subscriber_number_string = String.slice(phone_number, 6..9)
-
     "(#{area_code_string}) #{exchange_code_string}-#{subscriber_number_string}"
+  end
+
+  def capitalize_first_name(first_name) do
+    capital_first_name = String.capitalize(first_name)
+    "#{capital_first_name}"
+  end
+
+  def capitalize_last_name(last_name) do
+    capital_last_name = String.capitalize(last_name)
+    "#{capital_last_name}"
   end
 end
--- lib/dogger_web/views/pet_view.ex
+++ lib/dogger_web/views/pet_view.ex
@@ -1,3 +1,18 @@
 defmodule DoggerWeb.PetView do
   use DoggerWeb, :view
+
+  def capitalize_name(name) do
+    capital_name = String.capitalize(name)
+    "#{capital_name}"
+  end
+
+  def capitalize_owner_first_name(first_name) do
+    capital_first_name = String.capitalize(first_name)
+    "#{capital_first_name}"
+  end
+
+  def capitalize_owner_last_name(last_name) do
+    capital_last_name = String.capitalize(last_name)
+    "#{capital_last_name}"
+  end
 end
