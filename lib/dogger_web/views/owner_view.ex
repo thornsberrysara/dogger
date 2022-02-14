58e47644db899ab02cbef2777d37797e59e22534
thornsberrysara <thornsberrysara@gmail.com>
2022-02-13 20:06:47 -0500

    Formatted phone numbers

--- lib/dogger_web/views/owner_view.ex
+++ lib/dogger_web/views/owner_view.ex
@@ -1,3 +1,11 @@
 defmodule DoggerWeb.OwnerView do
   use DoggerWeb, :view
+
+  def format_phone_number(phone_number) do
+    area_code_string = String.slice(phone_number, 0..2)
+    exchange_code_string = String.slice(phone_number, 3..5)
+    subscriber_number_string = String.slice(phone_number, 6..9)
+
+    "(#{area_code_string}) #{exchange_code_string}-#{subscriber_number_string}"
+  end
 end
