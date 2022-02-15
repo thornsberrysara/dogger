cfae0f539e9d8d4fb7ddb1ec0e9d26c729ad96fe
thornsberrysara <thornsberrysara@gmail.com>
2022-02-15 09:44:51 -0500

    Fixed stay owner name capitalization

--- lib/dogger_web/views/stay_view.ex
+++ lib/dogger_web/views/stay_view.ex
@@ -1,3 +1,13 @@
 defmodule DoggerWeb.StayView do
   use DoggerWeb, :view
+
+  def capitalize_owner_first_name(first_name) do
+    capital_first_name = String.capitalize(first_name)
+    "#{capital_first_name}"
+  end
+
+  def capitalize_owner_last_name(last_name) do
+    capital_last_name = String.capitalize(last_name)
+    "#{capital_last_name}"
+  end
 end
